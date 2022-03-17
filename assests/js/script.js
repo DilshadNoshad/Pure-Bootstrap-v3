70d9619e674c5577e69cbad320f224376a11e73f
DilshadSheikh <dilshadnoshad2003@gmail.com>
2022-03-17 14:07:29 +0500

    update mission section

--- assests/js/script.js
+++ assests/js/script.js
@@ -12,3 +12,14 @@ $(window).scroll(function () {
     $(".nav-menu").removeClass("custom-navbar");
   }
 });
+
+$(window).scroll(function () {
+  let position = $(this).scrollTop();
+  if (position >= 650) {
+    $(".sunglass-img").addClass("slide_from_left");
+    $(".mission-text").addClass("slide_from_right");
+  } else {
+    $(".sunglass-img").removeClass("slide_from_left");
+    $(".mission-text").removeClass("slide_from_right");
+  }
+});
